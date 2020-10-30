5cf07c303c3294cbdcb55206e9656d454a0940eb
Gabriel <gabrieldiasbnu@hotmail.com>
2020-10-30 01:13:39 +0100

    Handle input file with limits, fileFilter, filename and destination

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,16 +1,24 @@
 import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+import { Request } from 'express';
 
 export default {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(req, file, callback) {
-      const hash = crypto.randomBytes(6).toString('hex');
-
+      const hash = crypto.randomBytes(6).toString('hex');            
       const fileName = `${hash}-${file.originalname}`;
-
       callback(null, fileName);
     }
   }),
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: function (req: Request, file: Express.Multer.File, cb:any) {  
+    const allowedMimeTypes = ['.jpg', '.jpeg', '.png','image/png','image/jpg','image/jpeg'];    
+    const fileExtension = path.extname(file.originalname).toLowerCase();        
+    if (allowedMimeTypes.indexOf(fileExtension) === -1) {      
+      return cb(new Error(`The file extension ${fileExtension} is not accepted.`), false)
+    }
+    cb(null, true)    
+  }
 }
