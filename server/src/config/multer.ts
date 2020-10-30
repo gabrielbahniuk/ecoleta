48b42361d5ef38bfca83b72925efce838d8e116d
Gabriel <gabrieldiasbnu@hotmail.com>
2020-10-30 08:57:31 +0100

    Check if file extension is empty - multer

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
@@ -7,18 +8,21 @@ export default {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(req, file, callback) {
-      const hash = crypto.randomBytes(6).toString('hex');            
+      const hash = crypto.randomBytes(6).toString('hex');
       const fileName = `${hash}-${file.originalname}`;
       callback(null, fileName);
     }
   }),
-  limits: { fileSize: 2 * 1024 * 1024 },
-  fileFilter: function (req: Request, file: Express.Multer.File, cb:any) {  
-    const allowedMimeTypes = ['.jpg', '.jpeg', '.png','image/png','image/jpg','image/jpeg'];    
-    const fileExtension = path.extname(file.originalname).toLowerCase();        
-    if (allowedMimeTypes.indexOf(fileExtension) === -1) {      
-      return cb(new Error(`The file extension ${fileExtension} is not accepted.`), false)
+  limits: { fileSize: Number(process.env.API_MAX_UPLOAD_FILESIZE_MB) * 1024 * 1024 },
+  fileFilter: function (req: Request, file: Express.Multer.File, cb:any) {
+    const allowedMimeTypes = ['.jpg', '.jpeg', '.png','image/png','image/jpg','image/jpeg'];
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+    if (!fileExtension) {
+      return cb(new Error(`The file extension cannot be empty.`), false);
     }
-    cb(null, true)    
+    if (allowedMimeTypes.indexOf(fileExtension) === -1) {
+      return cb(new Error(`The file extension ${fileExtension} is not accepted.`), false);
+    }
+    cb(null, true)
   }
 }
